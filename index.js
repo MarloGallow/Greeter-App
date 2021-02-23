3a6fa7fb0e8d19b97dd90f957fa17a4ceb340e0c
Marlo Gallow <mgallow226@noscihigh.org>
2021-02-23 01:15:39 +0000

    Finished

--- index.js
+++ index.js
@@ -7,15 +7,31 @@
  * // hour is 0-11
  * Good Morning!
  *
+ var time = "0 - 11";
+ if( time === "0 - 11") {
+   console.log("Good Morning")
+ }
  * // hour is 12-16
  * Good Afternoon! 
  *
+ var time = "12 - 16";
+ else if( time === "12 - 16") {
+   console.log("Good Afternoon")
+ }
  * // hour is 17-21
  * Good Evening!
  *
+ var time = "17 - 21";
+ else( time === "17 - 21") {
+   console.log("Good Evening")
+ }
  * // hour is 22-24 
  * Good Night!
  *
+ var time = "22 - 24";
+ if( time === "22 - 24") {
+   console.log("Good night")
+ }
  * TIPS: 
  * 
  *   a. At what threshold does morning become afternoon? What comparison 
@@ -28,3 +44,7 @@
  *   c. The tests are CASE sensitive, so you MUST console.log('Good Night!') with
  *      the exact case.
  */
+
+
+
+console.log(true)
